87b3a443d3f587d78a666fa8ff0d07588d70c02a
Sean.Rennie <sean.rennie6@gmail.com>
2019-10-01 15:23:49 +0200

    removed default CR css

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 import { LandingZone } from './components';
 
@@ -8,20 +6,6 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <LandingZone />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 };
