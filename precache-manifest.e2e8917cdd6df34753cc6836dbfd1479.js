590a52b757f4694a1412468f6a3c8b6154896964
Sean.Rennie <sean.rennie6@gmail.com>
2019-10-14 14:12:29 +0200

    Updates

---
+++ precache-manifest.e2e8917cdd6df34753cc6836dbfd1479.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a61c7e9c18deb146c97e6f07a0120446",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0819c67665bbccd8e81c",
+    "url": "/static/js/2.fdbf0154.chunk.js"
+  },
+  {
+    "revision": "79c583d4293ff256a6d6",
+    "url": "/static/js/main.add5028a.chunk.js"
+  },
+  {
+    "revision": "d63630fbc9b994a8ab3f",
+    "url": "/static/js/runtime-main.a534bbb8.js"
+  },
+  {
+    "revision": "c35561f5767ac29ae959c04255ce3d3b",
+    "url": "/static/media/TS-App-icon2.c35561f5.png"
+  },
+  {
+    "revision": "ef231a4b68a93779b90ceecb6c3d6bdc",
+    "url": "/static/media/battle-command-board-cropped-1200x1200.ef231a4b.png"
+  },
+  {
+    "revision": "ba323a1e0a732f1e8cf15a4e2c717a70",
+    "url": "/static/media/general-assembly-logo.ba323a1e.png"
+  },
+  {
+    "revision": "bada2e7a15c886e57e46f0e3e1580ceb",
+    "url": "/static/media/grit-dashboard-cropped-1200x1200.bada2e7a.png"
+  },
+  {
+    "revision": "e7c1c3deb5d5730a956cd5609784e8f9",
+    "url": "/static/media/livestock-manage-prototype-screens.e7c1c3de.png"
+  },
+  {
+    "revision": "a3a9d80e6b9a78f37790488d785b9e64",
+    "url": "/static/media/nice-rocks-show-page-cropped-1200x1200.a3a9d80e.png"
+  },
+  {
+    "revision": "ab54313355a51564b15b10302bee01d1",
+    "url": "/static/media/profile-picture-sean-small-sqr.ab543133.jpg"
+  },
+  {
+    "revision": "289e857099a7aac995777bbeb050493f",
+    "url": "/static/media/resolute-cropped-1200x1200.289e8570.png"
+  },
+  {
+    "revision": "5128acd6a75db9e08dc0db160f2a61c5",
+    "url": "/static/media/sr-logo-bricks-tagline.5128acd6.svg"
+  },
+  {
+    "revision": "02634974f5f47cd4912e6170353adddc",
+    "url": "/static/media/the-stockman-demo.02634974.gif"
+  }
+]);
